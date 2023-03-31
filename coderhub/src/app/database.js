b3e177dd7958ffc6badeb1a49d08e3bb9d206cee
liuguoqiang <liugq01@rd.netease.com>
2023-04-01 00:03:44 +0800

    mod: sync

--- coderhub/src/app/database.js
+++ coderhub/src/app/database.js
@@ -4,16 +4,11 @@ const config = require('./config');
 
 // 要注意, 服务器 Server 是另外的一套程序. 和自己经常玩的本地 DB 是不一样的. 
 const connections = mysql.createPool({
-  // host: config.MYSQL_HOST,
-  // port: config.MYSQL_PORT,
-  // database: config.MYSQL_DATABASE,
-  // user: config.MYSQL_USER,
-  // password: config.MYSQL_PASSWORD
-  host: 'localhost',
-  port: 3306,
-  database: 'codehub',
-  user: 'root',
-  password: 'Coderwhy888.'
+  host: config.MYSQL_HOST,
+  port: config.MYSQL_PORT,
+  database: config.MYSQL_DATABASE,
+  user: config.MYSQL_USER,
+  password: config.MYSQL_PASSWORD
 });
 
 connections.getConnection((err, conn) => {

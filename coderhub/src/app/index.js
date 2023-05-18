2a6a7c32bd20082fc1ed8d908b0cf825390c64e3
liuguoqiang <liugq01@rd.netease.com>
2023-05-18 18:14:51 +0800

    mod: 推上去最新的静态页面.

--- coderhub/src/app/index.js
+++ coderhub/src/app/index.js
@@ -1,4 +1,6 @@
 const Koa = require('koa');
+const path = require('path');
+const serve = require('koa-static');
 const bodyParser = require('koa-bodyparser');
 const errorHandler = require('./error-handle');
 const useRoutes = require('../router');
@@ -9,6 +11,9 @@ app.useRoutes = useRoutes;
 
 app.use(bodyParser());
 app.useRoutes();
+
+const publicPath = path.join(__dirname, '../public');
+app.use(serve(publicPath));
 
 // 所有的错误处理, 都是通过 error 进行的发送. 
 app.on('error', errorHandler);
